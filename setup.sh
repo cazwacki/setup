5b978c42c1c433e7af95094b11b78f6bdae75df8
Charles Zawacki <cazwacki@gmail.com>
2025-03-14 13:04:21 -0400

    fix: add quicktile to autostart

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 sudo apt-get install -y python3 python3-pip python3-setuptools python3-gi python3-xlib python3-dbus gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-wnck-3.0
 sudo -H pip3 install https://github.com/ssokolow/quicktile/archive/master.zip --break-system-packages
 cp ./quicktile.cfg ~/.config/quicktile.cfg
+cp Quicktile.desktop ~/.config/autostart/Quicktile.desktop
 
 # emacs for development
 sudo apt-get install emacs
