27a64e9b010c69834a07f700cf066e0a46a92cd0
Charles Zawacki <cazwacki@gmail.com>
2025-03-15 22:58:23 -0400

    style: reorganize

--- init.el
+++ init.el
@@ -1,59 +1,55 @@
 ;; run nerd-icons-install-fonts on first setup, then reload
+
+;; setup melpa and use-package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
-(setq backup-by-copying t)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
 
-;; quit Emacs directly even if there are running processes
-(setq confirm-kill-processes nil)
+;; keep customizations out of init
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-;; the toolbar is just a waste of valuable screen estate
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
+;;;;;;;;;;;;;;;;
+;; FUNCTIONAL ;;
+;;;;;;;;;;;;;;;;
 
-;; the blinking cursor is nothing, but an annoyance
-(blink-cursor-mode -1)
-
-;; DISABLE splash screen
-(setq inhibit-startup-screen t)
+(setq-default
+  default-directory "~/"
+  ;; always install packages that aren't present
+  use-package-always-ensure t
+  use-package-verbose t
+  ;; backups don't clutter the file tree
+  create-lockfiles nil
+  backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+  backup-by-copying t
+  delete-old-versions t
+  ;; quit Emacs directly even if there are running processes
+  confirm-kill-processes nil
+  ;; disable annoying startup stuff
+  inhibit-startup-screen t
+  initial-scratch-message nil
+  ;; newline at end of file
+  require-final-newline t
+  ;; use two spaces for indentation
+  indent-tabs-mode nil
+  tab-width 2
+  ;; wrap lines at 80 charaters
+  fill-column 80)
 
 ;; Automatically reread from disk if the underlying file changes
 (setopt auto-revert-avoid-polling t)
-
-(setopt line-number-mode t)           ;; Show current line in modeline
-(setopt column-number-mode t)         ;; Show column as well
-(pixel-scroll-precision-mode)         ;; Smooth scrolling
-
-;; built-in and fastest option these days
-(global-display-line-numbers-mode 1)
-(setopt display-line-numbers-width 3) ;; Set a minimum width
- 
-;; default indent to 2 spaces
-(setq-default indent-tabs-mode nil)   ;; don't use tabs to indent
-(setq-default tab-width 2)            ;; but maintain correct appearance
-
-;; Newline at end of file
-(setq require-final-newline t)
-
-;; Wrap lines at 80 characters
-(setq-default fill-column 80)
+(global-auto-revert-mode 1)
 
 (electric-pair-mode 1)                ;; Auto-close delimiters
 (delete-selection-mode 1)             ;; Delete highlighted region on type
-(show-paren-mode 1)                   ;; See matching character pairs           
-
-;;; use-package setup
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-
-(setq use-package-always-ensure t)
-(setq use-package-verbose t)
+(show-paren-mode 1)                   ;; See matching character pairs
 
 ;; Easily resize and cycle through frames, replacing the
 ;; buffer swap command -- rebound for colemak
@@ -74,20 +70,8 @@
   (win-switch-set-keys '("d") 'delete-window)
   (win-switch-set-keys '("-") 'split-vertically)
   (win-switch-set-keys '("|") 'split-horizontally)
-  (setq win-switch-idle-time 5))
+  (setq-default win-switch-idle-time 5))
 (global-set-key "\C-xo" 'win-switch-enter)
-
-;; which-key: shows a popup of available keybindings when typing a long key
-;; sequence (e.g. C-x ...)
-(use-package which-key
-  :config
-  (which-key-mode))
-
-(use-package dimmer)
-(dimmer-configure-which-key)
-(dimmer-configure-helm)
-(dimmer-mode t)
-(setq dimmer-fraction 0.4)
 
 ;; spatial-navigate: directional navigation through whitespace
 (use-package spatial-navigate)
@@ -96,28 +80,87 @@
 (global-set-key (kbd "<M-n>") 'spatial-navigate-backward-horizontal-bar)
 (global-set-key (kbd "<M-i>") 'spatial-navigate-forward-horizontal-bar)
 
-;; Use `nerd-icons' as Neotree's icon backend
-(use-package nerd-icons)
-(use-package neotree
-  :after (nerd-icons))
-(global-set-key [f8] 'neotree-toggle)
-(setq neo-theme (if (display-graphic-p) 'nerd-icons 'arrow))
-(setq-default neo-show-hidden-files t)
-(setq-default neo-autorefresh t)
-(setq-default neo-window-width 40)
-(setq-default neo-hide-cursor t)
-(neotree-toggle)
+;;;;;;;;;;;;;;;;
+;; APPEARANCE ;;
+;;;;;;;;;;;;;;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi))
- '(package-selected-packages '(nerd-icons use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; DISABLE SOME  annoying UI elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(blink-cursor-mode -1)
+(scroll-bar-mode -1)
+
+(setopt line-number-mode t)           ;; Show current line in modeline
+(setopt column-number-mode t)         ;; Show column as well
+(pixel-scroll-precision-mode)         ;; Smooth scrolling
+
+;; built-in and fastest option these days
+(global-display-line-numbers-mode 1)
+(setopt display-line-numbers-width 3) ;; Set a minimum width
+
+;; doom-themes: quality themes and some magic
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq-default
+    doom-themes-enable-bold t    
+    doom-themes-enable-italic t) 
+  (load-theme 'doom-challenger-deep t)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;; Use `nerd-icons' as icon backend for relevant packages
+(use-package nerd-icons)
+
+;; which-key: shows a popup of available keybindings when typing a long key
+;; sequence (e.g. C-x ...)
+(use-package which-key
+  :config
+  (which-key-mode))
+
+;; dimmer: dim all windows that aren't the currently selected one
+(use-package dimmer)
+(dimmer-configure-which-key)
+(dimmer-configure-helm)
+(dimmer-mode t)
+(setq-default dimmer-fraction 0.4)
+
+;; neotree: simple, pretty file navigation
+(use-package neotree
+  :after (nerd-icons)
+  :config
+  ;; disable line numbers for neotree
+  (add-hook 'neo-after-create-hook (lambda (&optional dummy) (display-line-numbers-mode -1)))
+  (setq-default
+    neo-theme (if (display-graphic-p) 'nerd-icons 'arrow)
+    neo-show-hidden-files t
+    neo-autorefresh t
+    neo-window-width 40
+    neo-hide-cursor t)
+  (global-set-key [f8] 'neotree-toggle)
+  (neotree-toggle))
+
+;; centaur-tabs: easy, intuitive tabs per window
+(use-package centaur-tabs
+  :demand
+  :init
+  (setq-default centaur-tabs-enable-key-bindings t)
+  :config
+  ;; enable and cycle through visible tabs
+  (centaur-tabs-mode t)
+  (setq-default centaur-tabs-cycle-scope 'tabs)
+  ;; order tabs alphabetically
+  (centaur-tabs-enable-buffer-alphabetical-reordering)
+  (setq-default centaur-tabs-adjust-buffer-order t)
+  (setq-default
+    centaur-tabs-set-icons t             ;; display icons on tabs
+    centaur-tabs-icon-type 'nerd-icons   ;; set icons to nerd-icons
+    centaur-tabs-set-modified-marker t   ;; display modification indicator
+    centaur-tabs-set-close-button nil    ;; hide close button
+    centaur-tabs-show-new-tab-button nil ;; hide new tab button
+    )
+  :bind
+  ("C-<tab>" . centaur-tabs-backward)
+  ("C-S-<tab>" . centaur-tabs-forward))
