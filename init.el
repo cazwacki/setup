c62825d885e1041c26d33e9238d5a68d04614f41
Charles Zawacki <cazwacki@gmail.com>
2025-03-17 18:58:23 -0400

    qol improvements

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; run nerd-icons-install-fonts on first setup, then reload
+;;; -*- lexical-binding: t -*-
 
 ;; setup melpa and use-package
 (require 'package)
@@ -27,9 +27,7 @@
   use-package-verbose t
   ;; backups don't clutter the file tree
   create-lockfiles nil
-  backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
-  backup-by-copying t
-  delete-old-versions t
+  make-backup-files nil
   ;; quit Emacs directly even if there are running processes
   confirm-kill-processes nil
   ;; disable annoying startup stuff
@@ -76,66 +74,58 @@
   (win-switch-set-keys '("|") 'split-horizontally)
   (global-set-key "\C-xo" 'win-switch-enter))
 
-;; spatial-navigate: directional navigation through whitespace
-(use-package spatial-navigate
-  :config
-  (global-set-key (kbd "<M-u>") 'spatial-navigate-backward-vertical-bar)
-  (global-set-key (kbd "<M-e>") 'spatial-navigate-forward-vertical-bar)
-  (global-set-key (kbd "<M-n>") 'spatial-navigate-backward-horizontal-bar)
-  (global-set-key (kbd "<M-i>") 'spatial-navigate-forward-horizontal-bar))
-
-;; scroll-on-jump: selectively add smooth scrolling to jumping commands
-(use-package scroll-on-jump
-  :custom
-  (scroll-on-jump-smooth t)
-  (scroll-on-jump-duration 0.4)
-  (scroll-on-jump-curve-power 4.0)
-  :config
-  ;; define actions to smooth
-  (scroll-on-jump-advice-add beginning-of-buffer)
-  (scroll-on-jump-advice-add end-of-buffer)
-  (scroll-on-jump-with-scroll-advice-add scroll-up)
-  (scroll-on-jump-with-scroll-advice-add scroll-down)
-  (scroll-on-jump-advice-add forward-paragraph)
-  (scroll-on-jump-advice-add backward-paragraph)
-  (scroll-on-jump-with-scroll-advice-add recenter-top-bottom))
-
 ;; avy: tree-based jump to char in visible buffer
 (use-package avy
   :custom
   (avy-keys '(?a ?r ?s ?t ?n ?e ?i ?o)) ;; only select with colemak home row
   (avy-style 'at-full)                  ;; obscure text on search to prevent text shifts
   (avy-all-windows t)                   ;; use all windows in search jump
-  :config
-  (global-set-key (kbd "C-'") 'avy-goto-char)
-  (global-set-key (kbd "C-\"") 'avy-goto-char-timer)
-  (global-set-key (kbd "C-:") 'avy-goto-word-0))
+  :bind
+  (("C-'" . avy-goto-char)
+   ("C-\"" . avy-goto-char-timer)
+   ("C-:" . avy-goto-word-0)))
 
 ;; ivy / counsel: enhanced completion for common emacs commands
 (use-package counsel
   :custom
   (ivy-wrap t)
   (ivy-height 10)
+  (ivy-use-virtual-buffers t)
   :config
+  (setq-default enable-recursive-minibuffers t)
   (ivy-mode 1)
-  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  :bind
+  (("C-c C-r" . ivy-resume)
   ;; ivy-based interfaces for standard commands
-  (global-set-key (kbd "C-s") 'swiper-isearch)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "M-y") 'counsel-yank-pop)
-  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-  (global-set-key (kbd "<f1> l") 'counsel-find-library)
-  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-  (global-set-key (kbd "<f2> j") 'counsel-set-variable)
-  (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
-  (global-set-key (kbd "C-c v") 'ivy-push-view)
-  (global-set-key (kbd "C-c V") 'ivy-pop-view)
+  ("C-s" . swiper-isearch)
+  ("M-x" . counsel-M-x)
+  ("C-x C-f" . counsel-find-file)
+  ("M-y" . counsel-yank-pop)
+  ("<f1> f" . counsel-describe-function)
+  ("<f1> v" . counsel-describe-variable)
+  ("<f1> l" . counsel-find-library)
+  ("<f2> i" . counsel-info-lookup-symbol)
+  ("<f2> u" . counsel-unicode-char)
+  ("<f2> j" . counsel-set-variable)
+  ("C-x b" . ivy-switch-buffer)
+  ("C-c v" . ivy-push-view)
+  ("C-c V" . ivy-pop-view)
   ;; ivy-based interfaces for shell tools
-  (global-set-key (kbd "C-x l") 'counsel-locate)
-  (global-set-key (kbd "C-c j") 'counsel-file-jump))
+  ("C-x l" . counsel-locate)
+  ("C-c j" . counsel-file-jump)))
+
+;; projectile: enable per-project capabilities
+(use-package projectile
+  :init
+  (projectile-mode +1)
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+
+;; flycheck: modern syntax checking
+(use-package flycheck
+  :demand
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
 
 ;;;;;;;;;;;;;;;;
 ;; APPEARANCE ;;
@@ -175,29 +165,8 @@
   (neo-hide-cursor t)
   :config
   ;; disable line numbers for neotree
-  (add-hook 'neo-after-create-hook (lambda (&optional dummy) (display-line-numbers-mode -1)))
-  (global-set-key [f8] 'neotree-toggle)
-  (neotree-toggle))
-
-;; centaur-tabs: easy, intuitive tabs per window
-(use-package centaur-tabs
-  :demand
-  :init
-  (setq-default centaur-tabs-enable-key-bindings t)
-  :custom
-  (centaur-tabs-cycle-scope 'tabs)       ;; enable and cycle through visible tabs
-  (centaur-tabs-adjust-buffer-order t)   
-  (centaur-tabs-set-icons t)             ;; display icons on tabs
-  (centaur-tabs-icon-type 'nerd-icons)   ;; set icons to nerd-icons
-  (centaur-tabs-set-modified-marker t)   ;; display modification indicator
-  (centaur-tabs-set-close-button nil)    ;; hide close button
-  (centaur-tabs-show-new-tab-button nil) ;; hide new tab button
-  :config
-  (centaur-tabs-mode t)
-  (centaur-tabs-enable-buffer-alphabetical-reordering) ;; order tabs alphabetically
-  :bind
-  ("C-<tab>" . centaur-tabs-backward)
-  ("C-<iso-lefttab>" . centaur-tabs-forward))
+  (add-hook 'neo-after-create-hook (lambda (&optional _) (display-line-numbers-mode -1)))
+  (global-set-key [f8] 'neotree-toggle))
 
 ;; doom-themes: quality themes and some magic
 (use-package doom-themes
@@ -227,3 +196,25 @@
     (left . 0.5))
   :config
   (mini-frame-mode))
+
+;; shell-pop: pop shell at the bottom of the frame
+(use-package shell-pop
+  :custom
+  (shell-pop-full-span t)
+  (shell-pop-universal-key "C-c s"))
+
+;; beacon: find your cursor
+(use-package beacon
+  :custom
+  (beacon-blink-when-point-moves-vertically 3)
+  (beacon-blink-when-focused t)
+  (beacon-color "#D6B4FC")
+  (beacon-size 10)
+  :config
+  (beacon-mode 1))
+
+;;;;;;;;;;;;;;;;;;;;
+;; DEFAULT LAYOUT ;;
+;;;;;;;;;;;;;;;;;;;;
+
+(neotree-toggle)
